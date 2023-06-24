410b823eff32ef393466be1c64c8dbf4c2f4466c
mdeluca13 <123423832+mdeluca13@users.noreply.github.com>
2023-06-24 14:58:01 -0400

    added portfolio page

--- src/components/pages/portfolio.js
+++ src/components/pages/portfolio.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import projects from '../projects';
+import github from '../../assets/images/github.svg';
+import '../styles/portfolio.css';
 
-export default function Portfolio(props) {
+export default function Portfolio() {
     return (
-        <ul className='list-group'>
-            {projects.map(project => (
-                <li className='list-group-item' key={project.id}>{project.title}</li>
-            )
-            )}
-        </ul>
+        <div className='container'>
+            <div className="row">
+                {projects.map(project => (
+                    <div className="col-sm-12 col-md-6 col-lg-6 col-xl-4  mt-5">
+                        <div className="card">
+                            <a href={project.deployed}><img className="card-img-top project-image" src={project.image} alt= {project.title}/></a>
+                            <h6 className="card-title">{project.title} <a href={project.github} className='icon'><img src={github} style={{ height: 23, width: 23 }} className='icon' alt='github icon' /></a> </h6>                        
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
     );
 };
--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import 'bootstrap/dist/css/bootstrap.min.css';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 ReactDOM.render(<App />, document.getElementById("root"));
