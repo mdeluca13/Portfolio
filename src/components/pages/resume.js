776eecddbfc09601d13d1c76bf2f0638cbea322f
mdeluca13 <123423832+mdeluca13@users.noreply.github.com>
2023-06-25 11:32:39 -0400

    added resume styling

--- src/components/pages/resume.js
+++ src/components/pages/resume.js
@@ -1,40 +1,45 @@
 import React from 'react';
-import pdf from '../../assets/files/DeLucaResume.pdf'
+import pdf from '../../assets/files/DeLucaResume.pdf';
+import download from '../../assets/images/download.png';
+import '../styles/resume.css';
 
 export default function Resume() {
     return (
         <div>
-            <a href={pdf} download>Download Resume</a>
-            <h2>Technical Skills</h2>
-            <ul className="list-group">
-                <li className="list-group-item">HTML</li>
-                <li className="list-group-item">CSS</li>
-                <li className="list-group-item">JavaScript</li>
-                <li className="list-group-item">Python</li>
-                <li className="list-group-item">MySQL</li>
-                <li className="list-group-item">NoSQL</li>
-                <li className="list-group-item">APIs</li>
-                <li className="list-group-item">Node.js</li>
-                <li className="list-group-item">OOP</li>
-                <li className="list-group-item">Express.js</li>
-                <li className="list-group-item">ORM</li>
-                <li className="list-group-item">MVC</li>
-                <li className="list-group-item">PWA</li>
-                <li className="list-group-item">React</li>
-                <li className="list-group-item">MERN</li>
-                <li className="list-group-item">State</li>
-                <li className="list-group-item">GraphQL</li>
-                <li className="list-group-item">Bootstrap</li>
-                <li className="list-group-item">Handlebars</li>
-                <li className="list-group-item">NPM</li>
-                <li className="list-group-item">Inquirer</li>
-                <li className="list-group-item">Sequelize</li>
-                <li className="list-group-item">GitHub</li>
-                <li className="list-group-item">Heroku</li>
-                <li className="list-group-item">Visual Studio Code</li>
-                <li className="list-group-item">Microsoft Suite</li>
-                <li className="list-group-item">Google Suite</li>
-            </ul>
+            <h1 className='resume-header'>Resume</h1>
+            <h5 className='resume-download'>Download Resume <a href={pdf} download><img src={download} style={{ height: 30 }} className='icon-download' alt='download icon' /></a></h5>
+            <h2 className='skills-header'>Technical Skills</h2>
+            <div className='skills-div'>
+                <ul className="list-group">
+                    <li className='list-group-item skill'>HTML</li>
+                    <li className='list-group-item skill'>CSS</li>
+                    <li className='list-group-item skill'>JavaScript</li>
+                    <li className='list-group-item skill'>Python</li>
+                    <li className='list-group-item skill'>MySQL</li>
+                    <li className='list-group-item skill'>NoSQL</li>
+                    <li className='list-group-item skill'>APIs</li>
+                    <li className='list-group-item skill'>Node.js</li>
+                    <li className='list-group-item skill'>OOP</li>
+                    <li className='list-group-item skill'>Express.js</li>
+                    <li className='list-group-item skill'>ORM</li>
+                    <li className='list-group-item skill'>MVC</li>
+                    <li className='list-group-item skill'>PWA</li>
+                    <li className='list-group-item skill'>React</li>
+                    <li className='list-group-item skill'>MERN</li>
+                    <li className='list-group-item skill'>State</li>
+                    <li className='list-group-item skill'>GraphQL</li>
+                    <li className='list-group-item skill'>Bootstrap</li>
+                    <li className='list-group-item skill'>Handlebars</li>
+                    <li className='list-group-item skill'>NPM</li>
+                    <li className='list-group-item skill'>Inquirer</li>
+                    <li className='list-group-item skill'>Sequelize</li>
+                    <li className='list-group-item skill'>GitHub</li>
+                    <li className='list-group-item skill'>Heroku</li>
+                    <li className='list-group-item skill'>Visual Studio Code</li>
+                    <li className='list-group-item skill'>Microsoft Suite</li>
+                    <li className='list-group-item skill'>Google Suite</li>
+                </ul>
+            </div>     
         </div>
     );
 };
