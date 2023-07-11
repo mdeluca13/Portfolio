5475b10729fb7bad6503ef0a826dd464f42a9675
mdeluca13 <123423832+mdeluca13@users.noreply.github.com>
2023-07-11 18:58:10 -0400

    adding resume link and styling to resume page

--- src/components/pages/resume.js
+++ src/components/pages/resume.js
@@ -11,6 +11,7 @@ export default function Resume() {
         <div>
             <h1 className='resume-header'>Resume</h1>
             <h5 className='resume-download'>Download Resume <a href={pdf} download><img src={download} style={{ height: 30 }} className='icon-download' alt='download icon' /></a></h5>
+            <a className='resume-link' href='https://docs.google.com/document/d/1JXLJOqDEAl6_WNlHDyT9Hnsy6avC1K9PW6oI34ZEWSc/edit?usp=sharing'>Visit Resume</a>
             <h2 className='skills-header'>Technical Skills</h2>
             <div className='skills-div'>
                 <ul className="list-group">
